ec6ce33c585766a73d378e2b2cea605650b5bd0f
DeflockJoplin <deflockjoplintoday@proton.me>
2026-01-05 02:47:43 -0600

    Make example root path generic

--- ALPR_Search.ps1
+++ ALPR_Search.ps1
@@ -4,8 +4,8 @@
    --------------------------------------------------------------- #>
 
 # ===================== USER SETTINGS ==============================
-$RootFolder = "C:\users\ndesi\Documents\JoplinScraper\joplin_council_pdfs"   # <-- change if needed
-$RunOCR     = $false   # set $true only the first time you need to OCR the PDFs
+$RootFolder = "C:\CouncilSearch\joplin_council_pdfs"   # <-- change if needed
+$RunOCR     = $true   # set $true only the first time you need to OCR the PDFs
 $OcrFolderName = "ocr_output"   # folder that will hold the OCR PDFs
 # ------------------------------------------------------------------
 
